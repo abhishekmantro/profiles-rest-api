dd613aa38136beb756e768eec2f80a39cc0069e2
Abhi <amantro@deloitte.com>
2025-07-03 18:35:18 +0530

    Added Search

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import filters
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
@@ -94,3 +95,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permision_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
